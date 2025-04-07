513fc0dd1733e8643a556337a89ce2f04872ee4c
Andrew Soltes <soltesandrew@gmail.com>
2025-04-07 20:46:42 +0800

    tst

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-POD_NAME=$(cat /etc/podinfo/podname)
-NODE_NAME=$(cat /etc/podinfo/nodename)
-
-sed "s/{{POD_NAME}}/$POD_NAME/" /usr/share/nginx/html/template.html | \
-sed "s/{{NODE_NAME}}/$NODE_NAME/" > /usr/share/nginx/html/index.html
-
 exec nginx -g 'daemon off;'
